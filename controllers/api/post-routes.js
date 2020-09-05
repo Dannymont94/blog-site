3272e56043ae249ae06c945ae8fead35f22c11bb
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-09-05 18:56:51 -0400

    created get and post routes for posts

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,93 @@
+const router = require('express').Router();
+const sequelize = require('../../config/connection');
+const { Post, User, Comment } = require('../../models');
+
+router.get('/', async (req, res) => {
+  try {
+    const dbPostData = await Post.findAll({
+      attributes: ['id', 'title', 'post_text', 'created_at'],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: [
+            {
+              model: User,
+              attributes: ['username']
+            }
+          ]
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    });
+
+    res.status(200).json(dbPostData);
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const dbPostData = await Post.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: ['id', 'title', 'post_text', 'created_at'],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    });
+
+    if (!dbPostData) {
+      res.status(400).json({ message: `No post found with this id` });
+      return;
+    }
+
+    res.status(200).json(err);
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const { title, post_text, user_id } = req.body;
+    if (!title || !post_text || !user_id) {
+      res.status(400).json({ message: 'Needs values for title, post_text, and user_id' });
+      return;
+    }
+
+    const dbPostData = await Post.create({
+      title,
+      post_text,
+      user_id
+    });
+
+    res.status(200).json(dbPostData);
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
