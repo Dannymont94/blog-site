6daeff9b4156ba005bc65c9f1fee0147b2c8c4c4
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-09-05 19:23:35 -0400

    created routes for comments

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,39 @@
+const router = require('express').Router();
+const { Comment } = require('../../models');
+const { post } = require('./user-routes');
+
+router.get('/', async (req, res) => {
+  try {
+    const dbCommentData = await Comment.findAll();
+    res.status(200).json(dbCommentData);
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const { comment_text, post_id } = req.body;
+    const { user_id } = req.session;
+    if (!comment_text || !post_id || !user_id) {
+      res.status(400).json({ message: `Needs values for comment_text, post_id, and user_id` });
+      return;
+    }
+
+    const dbCommentData = await Comment.create({
+      comment_text,
+      post_id,
+      user_id
+    });
+
+    res.status(200).json(dbCommentData);
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
