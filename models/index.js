bf3c2d63cf429c85c33a3dd57346d43189d705f7
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-09-05 17:16:11 -0400

    created comment model

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require('./User');
 const Post = require('./Post');
+const Comment = require('./Comment');
 
 User.hasMany(Post, {
   foreignKey: 'user_id',
@@ -11,4 +12,24 @@ Post.belongsTo(User, {
   onDelete: 'cascade'
 });
 
-module.exports = { User, Post };
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'cascade'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete:'cascade'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id',
+  onDelete: 'cascade'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id',
+  onDelete:'cascade'
+});
+
+module.exports = { User, Post, Comment };
