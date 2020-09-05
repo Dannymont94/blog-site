846274134daec0ca2c21a9d41fc964c1a8a4334d
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-09-05 16:08:45 -0400

    corrected some file path typos

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-const routes = require('controllers');
+const routes = require('./controllers');
 
-const sequelize = require('config/connection');
+const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
@@ -33,7 +33,7 @@ app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.status(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
