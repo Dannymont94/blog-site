d3e6f32a491a96d9c8a333907ba433a9b2dbd8c1
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-09-06 19:02:06 -0400

    fixed bug with adding new posts

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -1,8 +1,8 @@
 async function newPostFormHandler(event) {
   event.preventDefault();
 
-  const title = document.querySelector('input[name="post-title"]').value.trim();
-  const post_text = document.querySelector('input[name="post-text"]').value.trim();
+  const title = document.querySelector("#post-title").value.trim();
+  const post_text = document.querySelector("#post-text").value.trim();
 
   const response = await fetch('/api/posts', {
     method: 'POST',
